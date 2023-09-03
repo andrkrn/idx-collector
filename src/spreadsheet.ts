0d8434904c96aa82b6a6e402eec93a0ae5f6151d
Andri Kurnia <andrikurnia@live.com>
2023-09-03 23:38:35 +0700

    tweak google-spreadsheet implementation

--- src/spreadsheet.ts
+++ src/spreadsheet.ts
@@ -1,9 +1,16 @@
 import { GoogleSpreadsheet } from "google-spreadsheet";
 import { Stock } from "./idx";
+import { JWT } from "google-auth-library";
 
 const credentials = JSON.parse(
   process.env.CREDENTIALS ? process.env.CREDENTIALS : ""
 );
+
+const serviceAccountAuth = new JWT({
+  email: credentials.client_email,
+  key: credentials.private_key,
+  scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+});
 
 interface Data {
   [key: string]: string | number | boolean;
@@ -19,9 +26,12 @@ const HEADER_VALUES: HeaderValues = [
 ];
 class Spreadsheet {
   async updateStockPrice(rows: Data[]) {
-    const doc = new GoogleSpreadsheet(process.env.SHEET_ID);
+    const SHEET_ID = process.env.SHEET_ID;
 
-    await doc.useServiceAccountAuth(credentials);
+    if (!SHEET_ID) {
+      throw new Error("SHEET_ID is not defined");
+    }
+    const doc = new GoogleSpreadsheet(SHEET_ID, serviceAccountAuth);
 
     await doc.loadInfo();
 
